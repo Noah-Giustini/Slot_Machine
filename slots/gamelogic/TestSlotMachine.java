9805dd700bd487af5b61a3f287e9f2d6460c7e60
ebrintn <37120899+ebrintn@users.noreply.github.com>
2018-03-16 19:16:02 -0600

    Update TestSlotMachine.java

--- slots/gamelogic/TestSlotMachine.java
+++ slots/gamelogic/TestSlotMachine.java
@@ -26,21 +26,21 @@ public class TestSlotMachine {
     @Test
     public void test_constructors() {
         slots.gamelogic.Reel reel1 = new slots.gamelogic.Reel(0);
-        Reel reel2 = new Reel(1);
-        Reel reel3 = new Reel(2);
+        slots.gamelogic.Reel reel2 = new slots.gamelogic.Reel(1);
+        slots.gamelogic.Reel reel3 = new slots.gamelogic.Reel(2);
         assertEquals("Left reel needs location of 0", 0, reel1.getLocation());
         assertEquals("Middle reel needs location of 1", 1, reel2.getLocation());
         assertEquals("Right reel needs location of 2", 2, reel3.getLocation());
-        Game g = new Game(reel1, reel2, reel3);
+        slots.gamelogic.Game g = new slots.gamelogic.Game(reel1, reel2, reel3);
         assertEquals("Should have balance of $100", 100, g.getPlayerBalance());
     }
     
     @Test
     public void test_money() {
-        Reel reel1 = new Reel(0);
-        Reel reel2 = new Reel(1);
-        Reel reel3 = new Reel(2);
-        Game g = new Game(reel1, reel2, reel3);
+        slots.gamelogic.Reel reel1 = new slots.gamelogic.Reel(0);
+        slots.gamelogic.Reel reel2 = new slots.gamelogic.Reel(1);
+        slots.gamelogic.Reel reel3 = new slots.gamelogic.Reel(2);
+        slots.gamelogic.Game g = new slots.gamelogic.Game(reel1, reel2, reel3);
         assertEquals("Should have balance of $100", 100, g.getPlayerBalance());
         g.bet(33);
         assertEquals("Should have balance of $67 after betting $33", 67, g.getPlayerBalance());
@@ -49,11 +49,11 @@ public class TestSlotMachine {
     }
     
     @Test
-	public void test_getReels() {
-		Reel reel1 = new Reel(0);
-        Reel reel2 = new Reel(1);
-        Reel reel3 = new Reel(2);
-        Game g = new Game(reel1, reel2, reel3);
+    public void test_getReels() {
+	slots.gamelogic.Reel reel1 = new slots.gamelogic.Reel(0);
+        slots.gamelogic.Reel reel2 = new slots.gamelogic.Reel(1);
+        slots.gamelogic.Reel reel3 = new slots.gamelogic.Reel(2);
+        slots.gamelogic.Game g = new slots.gamelogic.Game(reel1, reel2, reel3);
         assertEquals("Reel1 should match", reel1.getReel()[0], g.getLeftList()[0]);
 	assertEquals("Reel1 should match", reel1.getReel()[1], g.getLeftList()[1]);
 	assertEquals("Reel1 should match", reel1.getReel()[2], g.getLeftList()[2]);
@@ -63,19 +63,19 @@ public class TestSlotMachine {
         assertEquals("Reel3 should match", reel3.getReel()[0], g.getRightList()[0]);
 	assertEquals("Reel3 should match", reel3.getReel()[1], g.getRightList()[1]);
 	assertEquals("Reel3 should match", reel3.getReel()[2], g.getRightList()[2]);
-	}
+    }
     
     @Test
-	public void test_rollReels() {
-		Reel reel1 = new Reel(0);
-        Reel reel2 = new Reel(1);
-        Reel reel3 = new Reel(2);
-        Game g = new Game(reel1, reel2, reel3);
-        g.rollAll();
-        assertTrue("Left reel needs to have all different items", testReel(reel1));
-        assertTrue("Middle reel needs to have all different items", testReel(reel2));
-        assertTrue("Reel reel needs to have all different items", testReel(reel3));
-	}
+    public void test_rollReels() {
+	slots.gamelogic.Reel reel1 = new slots.gamelogic.Reel(0);
+	slots.gamelogic.Reel reel2 = new slots.gamelogic.Reel(1);
+	slots.gamelogic.Reel reel3 = new slots.gamelogic.Reel(2);
+	slots.gamelogic.Game g = new slots.gamelogic.Game(reel1, reel2, reel3);
+	g.rollAll();
+	assertTrue("Left reel needs to have all different items", testReel(reel1));
+	assertTrue("Middle reel needs to have all different items", testReel(reel2));
+	assertTrue("Reel reel needs to have all different items", testReel(reel3));
+    }
     private boolean testReel(Reel reel) {
         int[] list = reel.getReel();
         if ((list[0] == list[1]) || (list[0] == list[2]) || (list[1] == list[2])) {
