5a68ef9aa1301c32f2ea1e6bdb1e276d6e91fd24
ebrintn <37120899+ebrintn@users.noreply.github.com>
2018-03-23 23:38:09 -0600

    Update SavedGame.java

--- slots/gamelogic/SavedGame.java
+++ slots/gamelogic/SavedGame.java
@@ -4,17 +4,30 @@ import java.io.*;
 
 
 
-/** The class SavedGame begins a game that has already been saved by the user. It takes in
-  * the past games balance and then makes it the starting balance of the next game.
+/** @author      Erin Brintnell
+  *  
+  * @version     1.0
+  *       
+  * The class SavedGame begins a game that has already been saved by the user. It takes in
+  * the past games balance and state and then makes a new game with this state. The Game class
+  * is inherited to run all of the game functions. Additionally this class does not roll the reels
+  * on the first round because the user has not interacted with the saved game yet.
+  *
+  * @var	isFirstRound - a boolean telling if this is the first time the user is 
+  *		interacting with the slot machine since the previous state has been
+  *		reloaded.
+  *
   */
 public class SavedGame extends Game{
 	boolean isFirstRound = true;
 	
 	/** Constructor
 	  * 
-	  * Initializes the super class and runs the read past game document. 
+	  * Initializes the super class and runs the readPastGame method changing each of the reels
 	  *
-	  * @param 	l, m, r - instances of the reel class
+	  * @param 	l - the leftmost reel for the game setup
+	  * @param	m - the middle reel for the game setup
+	  * @param	r - the right reel for the game setup
 	  *
 	  */
 	public SavedGame(Reel l,Reel m,Reel r){
@@ -26,7 +39,7 @@ public class SavedGame extends Game{
 	/** Overridden rollAll method. This method runs the super classes rollAll method 
 	  * if the round is not the first round being played. Otherwise nothing happens
 	  * besides changing the isFirst variable to false. This makes sure that the 
-	  * saved reels are displayed.
+	  * saved reels are displayed for the first game.
 	  *
 	  */
 	public void rollAll(){
@@ -39,15 +52,24 @@ public class SavedGame extends Game{
 	}
 	
 	
+	
 	/** The method readPastGame, reads a text file whih has all of the information
 	  * from the past game written to a buffered reader is used to do the reading
 	  * for each line of the saveFile. The the balance and values of the reels 
-	  * are then set according to the text document
+	  * are then set according to the text document which holds a previous state
+	  * of the game.
 	  *
-	  * @param 	1,m,r the three reels of the game
+	  * @var	lineRead -  the String read from each line of the document
+	  * @var	in - a buffered reading the incoming file IO
+	  * @var	numLinesRead - the number of lines read by the BufferedReader
+	  * @var	lReelValues - the values representing the state of the leftmost reel
+	  * @var	mReelValues - the values representing the state of the middle reel
+	  * @var	rReelValues - the values representing the state of the rightmost reel
+	  * @param 	l - the leftmost reel for the game setup
+	  * @param	m - the middle reel for the game setup
+	  * @param	r - the right reel for the game setup
 	  */
 	private void readPastGame(Reel l, Reel m, Reel r){
-		//Initializes necessary variables
 		String lineRead = "";
 		BufferedReader in = null;
 		int numLinesRead = 0;
@@ -100,12 +122,20 @@ public class SavedGame extends Game{
 			r.setReelValues(rReelValues);
 		}
 		
-		catch (IOException i){
-			System.out.println("exception");
+		catch (FileNotFoundException e){
+			System.out.println("I'm sorry I can't find a saved file, loading new game.");
+			super.rollAll();
 		}
 		
-		catch (Exception e){
+		catch (IOException e){
+			System.out.println("An error has occured in loading the saved game, loading new game.");
 			super.rollAll();
+		}
+		
+		catch (ArrayIndexOutOfBoundsException e){
+			System.out.println("There seems to be a problem with the state of the saved file, loading new game.");
+			super.rollAll();
+			
 		}	
 		
 		
