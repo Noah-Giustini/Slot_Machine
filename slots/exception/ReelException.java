e21bf417cca592620fbf9ca1d6d2e114ba4ec65e
ebrintn <37120899+ebrintn@users.noreply.github.com>
2018-03-25 21:30:43 -0600

    Update ReelException.java

--- slots/exception/ReelException.java
+++ slots/exception/ReelException.java
@@ -1,1 +1,45 @@
 
+package slots.exception;
+
+/**
+  * @author  Erin Brintnell
+  *  
+  * @version     1.0
+  *       
+  * The ReelException class inherits the exception class and is thrown whenever the reels are not set up 
+  * properly on the gameboard.
+  *
+  */
+public class NegativeBalanceException extends Exception{
+    /** Default Constructor
+      *
+      * No parameters are given when the exception is thrown. Thus there is no detailed message about the 
+      * exception, thus the super class is called with no exception message.
+      */
+    public NegativeBalanceException(){
+        super();
+    }
+  
+    /** Constructor
+      *
+      * A string with a message is given when the exception is thrown making the message about the exception
+      * slightly more detailed. The super class is then called with this message.
+      *
+      * @param      message - the detailed message about the exception.
+      *
+      */
+    public NegativeBalanceException(String message){
+        super(message);
+    }
+  
+    /** The exitTextGame method graciously exits out of the text version of the game printing a statement 
+      * telling the user that there is an error in the reels of the game.
+      *
+      */
+    public void exitTextGame(){
+        System.out.println("There seems to be an error in the reels of the slot machine, the program will now close...");
+        System.exit();
+    }
+
+}
+
