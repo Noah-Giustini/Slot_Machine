6891debc202d4ca70790208724666cac229a1779
ebrintn <37120899+ebrintn@users.noreply.github.com>
2018-04-01 17:19:44 -0600

    Update FXMLDocumentController.java

--- slots/guigame/FXMLDocumentController.java
+++ slots/guigame/FXMLDocumentController.java
@@ -55,8 +55,6 @@ import slots.exception.ReelException;
   * @var	hold - an integer counting how many times the user has pressed the hold button starting with 1 time
   * @var	holdStatus - a string showing the status of the hold
   */
-  
-  
 public class FXMLDocumentController implements Initializable {
      
         @FXML
@@ -107,7 +105,8 @@ public class FXMLDocumentController implements Initializable {
         @FXML
         private Label holdStatusLabel;
         
-        //Set up the reel and game objects
+
+	
         private Reel lReel = null;
         private Reel mReel = null;
         private Reel rReel = null;
@@ -115,85 +114,177 @@ public class FXMLDocumentController implements Initializable {
         private int hold = 1;
         private String holdStatus;
 
+	
+	
+	/** The method initialize sets up the game when the FXMLDocument controller class is orginally started.
+	  * the method begins by trying to set up the reels for the game. If a reel excepion is caught, meaning
+	  * that there is an error in the reels than the method displays a message to the user using the
+	  * holdStatusLabel and quits the application. The method sets all game objects to invisible 
+	  * while keeping objects needed for the starting screen visible. Finally it sets the start screen image 
+	  * to the title image.
+	  *
+	  * @param	url - the url of the FXMLDocument that is read by the FXMLDocument controller
+	  * @param	rb - the resource bundle needed to run the FXMLDocumentController class
+	  */
         @Override
         public void initialize(URL url, ResourceBundle rb) {
-                //At the beginning of the game when the user is choosing which game to playNormalGame
-                // for this reason the bet buttons are invisible
+		bet30.setVisible(false);
+                bet20.setVisible(false);
+                bet10.setVisible(false);
+                balance.setVisible(false);
+                save.setVisible(false);
+		holdButton.setVisible(false);
+		
 		try{
 			lReel = new Reel(0);
         		mReel = new Reel(1);
        			rReel = new Reel(2);
 		}
 		catch (ReelException e){
+			holdStatusLabel.setText("There seems to be an error with the reels of the game, the application"
+						+ "will now close");
 			System.exit(0);
 		}
-                bet30.setVisible(false);
-                bet20.setVisible(false);
-                bet10.setVisible(false);
-                balance.setVisible(false);
-                save.setVisible(false);
-		holdButton.setVisible(false);
+		
 		holdStatusLabel.setVisible(false);
 		
 		title.setImage(new Image("title.png"));
         }
 		
-        //Sets up a normal game that has (ie. not a saved game)
+	
+	
+        /** The method, playNormalGame, sets up a normal game that has not yet been played (ie. not a saved game)
+	  * it does this by setting the backend to a new Game with the three reels and rolling all of the reels
+	  * it then calls to the startGame method to begin the game.
+	  *
+	  * @param	event - the action event of pressing the normalGame button to start a new game
+	  */
         @FXML
         private void playNormalGame (ActionEvent event){
                 backend = new Game(lReel, mReel, rReel);
 		backend.rollAll();
 		startGame(event);
         }
-
-        //Sets up a game from a save file
+	
+	
+	
+        /** The method, playSavedGame, sets up a saved game from a saved file
+	  * it does this by setting the backend to a new SavedGame with the three reels, and typecasting backend to 
+	  * a saved game. It then calls to the startGame method to begin the game.
+	  *
+	  * @param	event - the action event of pressing the savedGame button to start a new game
+	  */
         @FXML
         private void playSavedGame (ActionEvent event){
                 backend = new SavedGame(lReel, mReel, rReel);
 		backend = (SavedGame) backend;
                 startGame(event);
         }
-		
+	
+	
+	
+	/** The method bet10Click, is called when the user presses the bet10 button. When this happens the method
+	  * calls to the betClick method with an amount to be bet of $10
+	  *
+	  * @param	event - the action event of pressing the bet10 button to bet $10
+	  *
+	  */
         @FXML
         private void bet10Click(ActionEvent event) {
             betClick(event, 10); //When you bet 10
         }
+	
+	
+	
+	/** The method bet20Click, is called when the user presses the bet20 button. When this happens the method
+	  * calls to the betClick method with an amount to be bet of $20
+	  *
+	  * @param	event - the action event of pressing the bet20 button to bet $20
+	  *
+	  */
         @FXML
         private void bet20Click(ActionEvent event) {
             betClick(event, 20); //When you bet 20
         }
+	
+	
+	
+	/** The method, bet30Click, is called when the user presses the bet30 button. When this happens the method
+	  * calls to the betClick method with an amount to be bet of $30
+	  *
+	  * @param	event - the action event of pressing the bet30 button to bet $30
+	  *
+	  */
         @FXML
         private void bet30Click(ActionEvent event) {
             betClick(event, 30); //When you bet 30
         }
-		
+	
+	
+	
+	/** The method, saveClick, is called when the user presses the save button. When this happens the method
+	  * saves the game in the backend variable
+	  *
+	  * @param	event - the action event of pressing the save button to save the game
+	  *
+	  */
         @FXML
         private void saveClick(ActionEvent event){
-                backend.saveGame(); //game is saved
+                backend.saveGame();
+        }
+	
+	
+	
+	/** The method, holdClick, is called when the user presses the hold button on the gui. When this happens
+	  * the hold integer value increments meaning that the button has been pressed one more time. With this
+	  * new value the status of the hold is calculated using the statusCalculate method and the holdStatusLabel
+	  * is set to the holdStatus string
+	  */
+	@FXML
+        private void holdClick(ActionEvent event){
+                this.hold++;
+                statusCalculate();
+                this.holdStatusLabel.setText(holdStatus);
         }
 
-        //This method removes the starting buttons and brings up the betting buttons as well as showing the game
-        private void startGame(ActionEvent event){
+	
+	
+        /** This method, startGame, removes the starting buttons and the introscreen by manking them invisible and 
+	  * brings up the betting buttons as well as showing the game, through the showScreen method
+	  *
+	  */
+	
+        private void startGame(){
                 bet30.setVisible(true);
                 bet20.setVisible(true);
                 bet10.setVisible(true);
                 balance.setVisible(true);
                 save.setVisible(true);
-                savedGame.setVisible(false);
-                normalGame.setVisible(false);
-		title.setVisible(false);
 		holdButton.setVisible(true);
 		holdStatusLabel.setVisible(true);
-		showScreen(event);
+		
+		savedGame.setVisible(false);
+                normalGame.setVisible(false);
+		title.setVisible(false);
+		
+		showScreen();
         }
 
-        //Shows the screen where all the images etc. are
-        private void showScreen(ActionEvent event){
-
-                //Change the text of the reel
+	
+	
+        /** The method, showScreen, shows the screen where all the images, buttons, etc. at the start of the game and 
+	  * after each time that the user has bet some money and the reels have rolled. The method first chances the 
+	  * balance variable to the new player balance in the backend variable and then sets each of the item image
+	  * views to the image of their respective item from the backend variable by getting the reel of their 
+	  * area and choosing a position in the array that is returned. The position in 0 of this array is the top
+	  * placement, the item in position 1 of the array is the middle item and the item in position 2 is the bottom
+	  * item.
+	  */
+        private void showScreen(){
+	
                 this.balance.setText("Balance: $"+Double.toString(backend.getPlayerBalance()));
 
-                //Set up the new images
+
                 lTop.setImage(new Image(Integer.toString(lReel.getReel()[0])+".png"));
                 lMid.setImage(new Image(Integer.toString(lReel.getReel()[1])+".png"));
                 lBot.setImage(new Image(Integer.toString(lReel.getReel()[2])+".png"));
@@ -206,12 +297,17 @@ public class FXMLDocumentController implements Initializable {
                 rMid.setImage(new Image(Integer.toString(rReel.getReel()[1])+".png"));
                 rBot.setImage(new Image(Integer.toString(rReel.getReel()[2])+".png"));
         }
-        @FXML
-        private void holdClick(ActionEvent event){
-                this.hold++;
-                statusCalculate();
-                this.holdStatusLabel.setText(holdStatus);
-        }
+	
+	
+	
+        /** The method, statusCalculate, calculates whether the middle reel needs to be held or not
+	  * if the middle reel has been pressed an odd number of times than the method will set
+	  * the holdStatus string to "Held", otherwise it will set the holdStatus string to "NotHeld".
+	  * The number of times that the hold status button has been pressed is odd if the hold integer
+	  * is even because the hold integer value begins as 1 with each game. In this manner the 
+	  * method uses an if statement to check for a number with no remainder when divided by 2 and 
+	  * if their is no remainder than the middle reel is to be held
+	  */
         private void statusCalculate(){
                 if ((this.hold % 2) == 0){
                     this.holdStatus = "Held";
@@ -221,6 +317,12 @@ public class FXMLDocumentController implements Initializable {
                 }
         }
 		
+	
+	
+	/** The method, betClick, is called whenever a user bets any amount of money through one of the
+	  * three betting buttons. The method uses that backend variable to check how much money will be
+	  * left if the game 
+	  */
         private void betClick(ActionEvent event, int amount) {
                 //This is the method that is called when you bet an amount
                 //The amount variable is the amount of the bet (can be 10, 20, or 30)
@@ -247,7 +349,7 @@ public class FXMLDocumentController implements Initializable {
                                 }
                         }
 						
-			showScreen(event);
+			showScreen();
                         
                         
                 }
