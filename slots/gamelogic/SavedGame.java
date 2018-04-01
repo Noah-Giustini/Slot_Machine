36a3378c1e38bf855eea0dd41cda601816e6823d
ebrintn <37120899+ebrintn@users.noreply.github.com>
2018-03-31 20:29:01 -0600

    Update SavedGame.java

--- slots/gamelogic/SavedGame.java
+++ slots/gamelogic/SavedGame.java
@@ -99,7 +99,7 @@ public class SavedGame extends Game{
 				
 				//The next three lines are the values of the left reel, which are added
 				// to the left reel array
-				else if (numLinesRead < 4){
+				else if (numLinesRead <= 3){
 					lReelValues[numLinesRead -1] = Integer.parseInt(lineRead);
 					numLinesRead ++;
 				}
@@ -107,15 +107,15 @@ public class SavedGame extends Game{
 				
 				//The next three lines are the values of the middle reel, which are added
 				// to the middle reel array
-				else if (numLinesRead < 6){
-					mReelValues[numLinesRead - 4] = Integer.parseInt(lineRead);
+				else if (numLinesRead <= 6){
+					mReelValues[numLinesRead - 3] = Integer.parseInt(lineRead);
 					numLinesRead ++;
 				}
 				
 				//The last three lines are the values of the right reel, which are added
 				// to the right reel array
-				else if (numLinesRead < 10){
-					rReelValues[numLinesRead - 7] = Integer.parseInt(lineRead);
+				else if (numLinesRead <= 9){
+					rReelValues[numLinesRead - 6] = Integer.parseInt(lineRead);
 					numLinesRead ++;
 				}
 				System.out.println(Integer.parseInt(lineRead));	
