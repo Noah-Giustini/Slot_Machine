a839d0a84a31b40ab50c1aa0e96037b9a7d674ca
ebrintn <37120899+ebrintn@users.noreply.github.com>
2018-04-01 16:40:58 -0600

    Update WeightedRandom.java

--- slots/gamelogic/WeightedRandom.java
+++ slots/gamelogic/WeightedRandom.java
@@ -25,7 +25,7 @@ public class WeightedRandom {
       */
     public WeightedRandom(ArrayList<Item> i) {
         for (Item item: i){
-            items.add(new Item( i.getWieght()));
+            items.add(new Item( i.getWeight()));
         }
     }
  
