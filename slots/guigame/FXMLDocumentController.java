3b922af53e3c5171624d894dca08e4aa2ccb9ab7
ebrintn <37120899+ebrintn@users.noreply.github.com>
2018-04-01 18:20:33 -0600

    Update FXMLDocumentController.java

--- slots/guigame/FXMLDocumentController.java
+++ slots/guigame/FXMLDocumentController.java
@@ -22,6 +22,7 @@ import javafx.scene.layout.HBox;
 import java.lang.Integer;
 import java.lang.Double;
 import javafx.scene.control.CheckBox;
+import javafx.scene.shape.Rectangle;
 import slots.gamelogic.*;
 import slots.exception.ReelException;
 
