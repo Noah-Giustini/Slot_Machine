fde503f3d0f52435db85663104d1118b8dd73710
ebrintn <37120899+ebrintn@users.noreply.github.com>
2018-04-08 16:36:28 -0600

    Update ReelException.java

--- slots/exception/ReelException.java
+++ slots/exception/ReelException.java
@@ -1,39 +1,35 @@
-
 package slots.exception;
 
 /**
-  * @author  Erin Brintnell
-  *  
-  * @version     1.0
-  *       
-  * The ReelException class inherits the exception class and is thrown whenever the reels are not set up 
-  * properly on the gameboard.
-  *
-  */
-public class ReelException extends Exception{
- 
+ * @author  Erin Brintnell
+ *  
+ * @version     1.0
+ *       
+ * The ReelException class inherits the exception class and is thrown whenever the reels are not set up 
+ * properly on the gameboard.
+ *
+ */
+public class ReelException extends Exception {
+
     /** Default Constructor
-      *
-      * No parameters are given when the exception is thrown. Thus there is no detailed message about the 
-      * exception, thus the super class is called with no exception message.
-      */
-    public ReelException(){
+     *
+     * No parameters are given when the exception is thrown. Thus there is no detailed message about the 
+     * exception, thus the super class is called with no exception message.
+     */
+    public ReelException() {
         super();
     }
-  
+
     /** Constructor
-      *
-      * A string with a message is given when the exception is thrown making the message about the exception
-      * slightly more detailed. The super class is then called with this message.
-      *
-      * @param      message - the detailed message about the exception.
-      *
-      */
-    public ReelException(String message){
+     *
+     * A string with a message is given when the exception is thrown making the message about the exception
+     * slightly more detailed. The super class is then called with this message.
+     *
+     * @param      message - the detailed message about the exception.
+     *
+     */
+    public ReelException(String message) {
         super(message);
     }
-  
-
-
 }
 
