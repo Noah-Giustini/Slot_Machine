c97a5e617795165e97de206b159c464c8bc95ac8
Noah Giustini <36166847+Noah-Giustini@users.noreply.github.com>
2018-04-09 11:42:38 -0600

    Slot_Machine
    more changes to the .at and .sh files. I have also created ones that can launch the testing

--- runmeLNX.sh
+++ runmeLNX.sh
@@ -2,6 +2,5 @@ javac slots/rungame/*.java
 javac slots/guigame/*.java
 javac slots/gamelogic/*.java
 javac slots/exception/*.java
-javac slots/test/*.java
 clear
 java slots.rungame.Play
--- runmeWNDS.bat
+++ runmeWNDS.bat
@@ -3,6 +3,5 @@ javac slots/rungame/*.java
 javac slots/guigame/*.java
 javac slots/gamelogic/*.java
 javac slots/exception/*.java
-javac slots/test/*.java
 cls
 java slots.rungame.Play
